f65d7dacb9dd1534512e418de6c3ee6e99e63940
oriporatt <oriporatt@gmail.com>
2024-11-27 22:46:02 +0200

    update5 pictures

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -9,9 +9,9 @@ export function BookPreview({ book }) {
             <h2>Title: {book.title}</h2>
             <h4> Price: {book.listPrice.amount} {book.listPrice.currencyCode}</h4>
             <h5> Price $: {book.listPrice.dollarPrice}</h5>
-            <img src={`assets/img/d01.jpg`} alt="book-image" />
 
-            {/* <img src={`assets/img/${book.thumbnail}`} alt="book-image" /> */}
+            <img src={`/assets/img/${book.thumbnail}`} alt="book-image" />
         </article>
     )
 }
+
