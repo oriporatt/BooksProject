b603053ab8dafad55639026fb81123eaa4a99520
oriporatt <oriporatt@gmail.com>
2024-11-27 22:27:48 +0200

    update home page2

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -4,7 +4,7 @@ export function Home() {
     return (
         <section className="home">
             <h1> Welcome to MyBooks app...</h1>
-            <img src="/assets/img/home.jpg" alt="book-image" />
+            <img src="assets/img/home.jpg" alt="book-image" />
 
        </section>
     )
