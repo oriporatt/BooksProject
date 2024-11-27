1dc36d248ea7ca4fdcb02c218aee8ebb33b0fc28
oriporatt <oriporatt@gmail.com>
2024-11-27 21:30:58 +0200

    back to origin

--- react-template-proj/app.js
+++ react-template-proj/app.js
@@ -3,6 +3,3 @@ import { RootCmp } from './RootCmp.jsx'
 const elContainer = document.getElementById('root')
 const root = ReactDOM.createRoot(elContainer)
 root.render(<RootCmp />)
-
-
-//check 2128
