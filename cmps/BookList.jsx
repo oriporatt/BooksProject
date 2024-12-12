316df97711992dcb22530a0891eb017d23a50a84
oriporatt <oriporatt@gmail.com>
2024-12-12 11:28:03 +0200

    last update

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -16,7 +16,6 @@ export function BookList({ books, removeBook }) {
                         <button onClick={()=>removeBook(book.id)} > Remove</button>
                         <button ><Link to={`/book/${book.id}`}>Details</Link></button>
                         <button ><Link to={`/book/edit/${book.id}`}>Edit</Link></button>
-         
                     </section>
                 </li>
             )}
